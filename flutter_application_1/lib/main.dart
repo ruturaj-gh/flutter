6c8113ffa3ce610191f66c5b2d86e1e61eaf1914
Ruturaj <rutughumkar33@gmail.com>
2022-05-15 18:37:53 +0530

    chapter 2 done

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,13 +1,10 @@
 // import 'package:flutter/widgets.dart';
 
-
-
 import 'package:flutter/material.dart';
 
 // import 'package:flutter/cupertino.dart';
 void main() {
-  runApp(const MaterialApp(title: "Awesome app", 
-  home: Homepage()));
+  runApp(const MaterialApp(title: "Awesome app", home: Homepage()));
 }
 
 class Homepage extends StatelessWidget {
@@ -16,10 +13,45 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Flutter app"),
-      ),
-      body: const Center(child: Text("hiii flutter")),
-    );
+        appBar: AppBar(
+          title: const Text("Flutter app"),
+        shadowColor: Colors.black,
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:<Widget>[
+            Container(
+            padding: const EdgeInsets.all(10),
+
+            width: 100,
+            height: 100,
+            color: Colors.red,
+            
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            
+
+            
+            width: 100,
+            height: 100,
+            color: Colors.green,
+            
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            
+
+            
+            width: 100,
+            height: 100,
+            color: Colors.blue,
+            
+          ),
+            ],
+          )
+        )
+        );
   }
 }
